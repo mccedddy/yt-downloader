b92c5de169ec4af997e6a2ec7a2a0ba11a3ffbc1
mccedddy <cedricmacallan@gmail.com>
2024-10-03 17:47:56 +0800

    added loader and dl icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import SearchIcon from "./assets/icons/search.svg";
+import DownloadIcon from "./assets/icons/download.svg";
 import YoutubeLogo from "./assets/images/youtube.jpg";
 
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col gap-4 justify-center items-center bg-customBlack">
-      <h1 className="text-2xl font-bold text-customWhite">YT Downloader</h1>
-      <div className="flex flex-col gap-4 items-center">
+    <div className="h-screen w-screen flex flex-col gap-8 justify-center items-center bg-customBlack">
+      <h1 className="text-4xl font-bold text-customWhite">YT Downloader</h1>
+      <div className="flex flex-col gap-8 items-center">
         {/* Input and Search Button */}
         <div className="flex">
           <input
@@ -18,27 +19,31 @@ function App() {
           </button>
         </div>
 
-        {/* Video Information */}
-        <div className="flex flex-col gap-2">
-          {/* Thumbnail */}
+        {/* Loader */}
+        <div className="flex flex-col justify-center items-center gap-8">
+          <div className="spinner"></div>
+          <h1 className="text-customWhite">Proecssing download link...</h1>
+        </div>
+
+        {/* Video */}
+        <div className="flex flex-col items-center gap-2">
           <img
             src={YoutubeLogo}
             alt="Video Thumbnail"
             className="w-96 h-54 object-cover rounded-lg"
           />
 
-          {/* Title and Duration */}
           <div className="flex justify-between w-full">
-            <h1 className="font-bold text-customWhite">
+            <h1 className="text-sm font-bold text-customWhite">
               Title Title Title Title
             </h1>
-            <h1 className="text-customGray">3:45</h1>
+            <h1 className="text-sm text-customGray-lighter">3:45</h1>
           </div>
+          <button className="h-8 w-32 mt-2 pl-3 pr-4 flex justify-center items-center gap-2 rounded-lg bg-customRed text-customWhite">
+            <img src={DownloadIcon} className="h-4 w-4" alt="download icon" />
+            Download
+          </button>
         </div>
-
-        <button className="h-8 px-4 rounded-lg bg-customRed text-customWhite">
-          Download
-        </button>
       </div>
     </div>
   );
